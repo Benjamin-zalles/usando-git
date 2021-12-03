9ec660e8eec0c90276bdf674dae96d2eb9f6f5bb
Benjamin <benjaminzalles@gmail.com>
2021-12-03 00:12:05 -0400

    usando javascript

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("hola mundo");
+
+function modificarTexto() {
+    var dark = document.getElementById("light");
+    dark.firstChild.nodeValue = "Modo Oscuro";
+};
+
+function myFuntion() {
+    var element = document.getElementById("caja");
+    element.classList.toggle("darkmode");
+};
+
+
+
+// document.addEventListener("click", modificarTexto);
+
+
+// function darkMode() {
+//     var oscuro = document.getElementById("dark").style.display = "block";
+//     var light = document.getElementById("light").style.display = "none";
+// };
+
+
+
+
+// alert("hola");
+
