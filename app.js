25965485ea718d62fac7217b0f966fe180462326
Benjamin <benjaminzalles@gmail.com>
2021-12-03 08:56:44 -0400

    nuevo2

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 
-function modificarTexto() {
-    var dark = document.getElementById("light");
-    dark.firstChild.nodeValue = "Modo Oscuro";
-};
 
 function myFuntion() {
     var element = document.getElementById("caja");
     element.classList.toggle("darkmode");
 };
 
-
+function modificarTexto() {
+    var dark = document.getElementById("light");
+    dark.firstChild.nodeValue = "Modo Oscuro";
+};
 
 
